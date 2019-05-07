d0120aed2c2a2f9944efcda35551fc2a8552a2a5
gfike19@gmail.com <Vaughn*573>
2019-05-07 14:07:49 -0500

    got add item to work as expected

--- src/main/java/com/gfike/SelfCheckoutSim/controllers/EditCartController.java
+++ src/main/java/com/gfike/SelfCheckoutSim/controllers/EditCartController.java
@@ -1,23 +1,16 @@
 package com.gfike.SelfCheckoutSim.controllers;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.AbstractController;
-
-import com.gfike.SelfCheckoutSim.Helpers;
 import com.gfike.SelfCheckoutSim.daos.ItemDao;
 import com.gfike.SelfCheckoutSim.models.Item;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 
 @Controller
 public class EditCartController {
@@ -25,67 +18,37 @@ public class EditCartController {
     @Autowired
     public ItemDao itemDao;
 
-    @RequestMapping(value = {"/editCart","redirect:editCart"}, method = RequestMethod.GET)
+    @RequestMapping(value ="/editCart", method = RequestMethod.GET)
     public String editCartGet(Model model, HttpSession session){
 
-        String msg = (String)session.getAttribute("msg");
-        model.addAttribute("msg", msg);
+        if (session.getAttribute("cart") != null) {
+            ArrayList<Item> cart = (ArrayList<Item>)session.getAttribute("cart");
+            model.addAttribute("cart", cart);
+        }
 
         List<Item> items = itemDao.findAll();
         model.addAttribute("items", items);
-
-        try {
-            ArrayList<Item> cart = (ArrayList<Item>) session.getAttribute("cart");
-            model.addAttribute("cart",cart);
-        } catch (Exception e) {
-            ArrayList<Item> cart = new ArrayList<Item>();
-            session.setAttribute("cart", cart);
-            model.addAttribute("cart","");
-        }
-
         return "editCart";
     }
 
     @RequestMapping(value="/editCart", method = RequestMethod.POST)
     public String addItem (Model model, ServletRequest request, HttpSession session) {
-        String exp = "";
-        try {
+
+        if (session.getAttribute("cart") != null) {
+            ArrayList<Item> cart = (ArrayList<Item>)session.getAttribute("cart");
             int id = Integer.parseInt(request.getParameter("shelf"));
-            String msg = "";
-            ArrayList<Item> cart = (ArrayList<Item>) session.getAttribute("cart");
             Item i = itemDao.findById(id);
             cart.add(i);
+        }
+        else {
+            int id = Integer.parseInt(request.getParameter("shelf"));
+            Item i = itemDao.findById(id);
+            ArrayList<Item> cart = new ArrayList<>();
+            cart.add(i);
             session.setAttribute("cart", cart);
-            msg = "Item has been added to cart!";
-            session.setAttribute("msg",msg);
-            model.addAttribute("msg", msg);
             model.addAttribute("cart", cart);
-            return "redirect:editCart";
         }
-        catch (Exception e) {
-            exp = e.toString();
-        }
-        model.addAttribute("exp", exp);
-        return "redirect:editCart";
+
+        return "redirect:/editCart";
     }
-//    @RequestMapping(value = "/addItem",method = RequestMethod.GET)
-//    public String addToCart(Model model, HttpSession session){
-//
-//        String msg = (String)session.getAttribute("msg");
-//        model.addAttribute("msg", msg);
-//
-//        List<Item> items = itemDao.findAll();
-//        model.addAttribute("items", items);
-//
-//        try {
-//            ArrayList<Item> cart = (ArrayList<Item>) session.getAttribute("cart");
-//            model.addAttribute("cart",cart);
-//        } catch (Exception e) {
-//            ArrayList<Item> cart = new ArrayList<Item>();
-//            session.setAttribute("cart", cart);
-//            model.addAttribute("cart","");
-//        }
-//
-//        return "editCart";
-//    }
 }
