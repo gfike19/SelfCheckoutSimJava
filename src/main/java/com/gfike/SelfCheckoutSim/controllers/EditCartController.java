67815917ce4a9b65769b15ef92473a373e5c227b
gfike19@gmail.com <Vaughn*573>
2019-09-06 12:25:09 -0500

    refactored add item to cart to be DRY, am able to pull id's from checkboxes

--- src/main/java/com/gfike/SelfCheckoutSim/controllers/EditCartController.java
+++ src/main/java/com/gfike/SelfCheckoutSim/controllers/EditCartController.java
@@ -1,8 +1,10 @@
 package com.gfike.SelfCheckoutSim.controllers;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.gfike.SelfCheckoutSim.daos.ItemDao;
 import com.gfike.SelfCheckoutSim.models.Item;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EditCartController {
@@ -28,28 +31,49 @@ public class EditCartController {
 
         List<Item> items = itemDao.findAll();
         model.addAttribute("items", items);
+
+        String msg = (String)session.getAttribute("msg");
+        model.addAttribute("msg", msg);
         return "editCart";
     }
     //TODO work on noting if an item has duplicates
     //TODO add validation
     @RequestMapping(value="/editCart", params="add", method = RequestMethod.POST)
     public String addItem (Model model, ServletRequest request, HttpSession session) {
+        Item i;
+        ArrayList<Item> cart;
+        int id = Integer.parseInt(request.getParameter("shelf"));
+        i = itemDao.findById(id);
 
         if (session.getAttribute("cart") != null) {
-            ArrayList<Item> cart = (ArrayList<Item>)session.getAttribute("cart");
-            int id = Integer.parseInt(request.getParameter("shelf"));
-            Item i = itemDao.findById(id);
-            cart.add(i);
+            cart = (ArrayList<Item>)session.getAttribute("cart");
         }
         else {
-            ArrayList<Item> cart = new ArrayList<>();
-            int id = Integer.parseInt(request.getParameter("shelf"));
-            Item i = itemDao.findById(id);
-            cart.add(i);
-            session.setAttribute("cart", cart);
-            model.addAttribute("cart", cart);
+            cart = new ArrayList<>();
         }
 
+        cart.add(i);
+        session.setAttribute("cart", cart);
+        model.addAttribute("cart", cart);
+        String msg = i.getName() + " has been added to the cart!";
+
+        session.setAttribute("msg", msg);
+        model.addAttribute("msg", msg);
+        return "redirect:/editCart";
+    }
+
+    @RequestMapping(value="/editCart", params="remove", method = RequestMethod.POST)
+    public String removeItems (Model model, HttpServletRequest request, HttpSession session,
+    @RequestParam List<String> markedItem) {
+
+        // doesn't work
+//        String [] checkboxes = request.getParameterValues("markedItem");
+//        model.addAttribute("msg", checkboxes.length);
+
+        String msg = markedItem.toString();
+        model.addAttribute("msg", msg);
+        session.setAttribute("msg", msg);
         return "redirect:/editCart";
     }
 }
+
