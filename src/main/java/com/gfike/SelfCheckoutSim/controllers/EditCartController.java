6efaa270a2df519518ce36798cb3739d484eb86e
gfike19@gmail.com <Vaughn*573>
2019-09-06 15:02:07 -0500

    issue is realted to session, see bookmarks for possible solution

--- src/main/java/com/gfike/SelfCheckoutSim/controllers/EditCartController.java
+++ src/main/java/com/gfike/SelfCheckoutSim/controllers/EditCartController.java
@@ -1,8 +1,8 @@
 package com.gfike.SelfCheckoutSim.controllers;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -20,8 +20,9 @@ public class EditCartController {
 
     @Autowired
     public ItemDao itemDao;
-
-    @RequestMapping(value ="/editCart", method = RequestMethod.GET)
+// doesn't work
+//    @RequestMapping(value ="/editCart",params={"","add"}, method = RequestMethod.GET)
+@RequestMapping(value ="/editCart", method = RequestMethod.GET)
     public String editCartGet(Model model, HttpSession session){
 
         if (session.getAttribute("cart") != null) {
@@ -36,6 +37,23 @@ public class EditCartController {
         model.addAttribute("msg", msg);
         return "editCart";
     }
+    // doesn't work
+//    @RequestMapping(value ="/editCart", params="remove", method = RequestMethod.GET)
+//    public String removeItemGet(Model model, HttpSession session){
+//
+//        if (session.getAttribute("cart") != null) {
+//            ArrayList<Item> cart = (ArrayList<Item>)session.getAttribute("cart");
+//            model.addAttribute("cart", cart);
+//            session.removeAttribute("cart");
+//        }
+//
+//        List<Item> items = itemDao.findAll();
+//        model.addAttribute("items", items);
+//
+//        String msg = (String)session.getAttribute("msg");
+//        model.addAttribute("msg", msg);
+//        return "editCart";
+//    }
     //TODO work on noting if an item has duplicates
     //TODO add validation
     @RequestMapping(value="/editCart", params="add", method = RequestMethod.POST)
@@ -65,12 +83,43 @@ public class EditCartController {
     @RequestMapping(value="/editCart", params="remove", method = RequestMethod.POST)
     public String removeItems (Model model, HttpServletRequest request, HttpSession session,
     @RequestParam List<String> markedItem) {
+        ArrayList<Item> cart;
+        // turns list of strings to list of Integers
+        // doesn't really work here though :(
+//        List<Integer> itemIds = markedItem.stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+        //List<String> itemNames = null;
+        /* TODO create error msg for when they are trying to remove when cart is empty */
+        if (session.getAttribute("cart") != null) {
+            cart = (ArrayList<Item>)session.getAttribute("cart");
+            // doesn't work
+//            session.removeAttribute("cart");
+        }
+        else {
+            cart = new ArrayList<>();
+        }
 
+        String msg = "";
+        // tried remapping cart in session to a new cart, didn't work
+        try {
+            for(String s : markedItem){
+                int i = Integer.parseInt(s);
+                Item itemToRemove = itemDao.findById(i);
+
+                if(cart.contains(itemToRemove)){
+                    cart.remove(itemToRemove);
+                }
+            }
+
+            msg = "Current cart size: " + cart.size();
+
+        } catch(Exception e) {
+            msg = e.toString();
+        }
         // doesn't work
-//        String [] checkboxes = request.getParameterValues("markedItem");
-//        model.addAttribute("msg", checkboxes.length);
+//        session.removeAttribute("cart");
+        session.setAttribute("cart", cart);
+        model.addAttribute("cart", cart);
 
-        String msg = markedItem.toString();
         model.addAttribute("msg", msg);
         session.setAttribute("msg", msg);
         return "redirect:/editCart";
