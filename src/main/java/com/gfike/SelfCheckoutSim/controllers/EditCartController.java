2af29dc02c3543b1cad262fc12a74210fe4611e5
Gail Fike <gfike19@gmail.com>
2019-09-18 21:07:31 -0500

    FIANLLY GOT REMOVE ITEM TO WORKgit add .git add .!

--- src/main/java/com/gfike/SelfCheckoutSim/controllers/EditCartController.java
+++ src/main/java/com/gfike/SelfCheckoutSim/controllers/EditCartController.java
@@ -1,18 +1,20 @@
 package com.gfike.SelfCheckoutSim.controllers;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
 import com.gfike.SelfCheckoutSim.daos.ItemDao;
 import com.gfike.SelfCheckoutSim.models.Item;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.support.SessionStatus;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class EditCartController {
@@ -67,8 +69,6 @@ public class EditCartController {
 
         session.setAttribute("msg", msg);
         model.addAttribute("msg", msg);
-        // doesn't work
-//        sessionStatus.setComplete();
         return "redirect:/editCart";
     }
 
@@ -82,23 +82,28 @@ public class EditCartController {
         /* TODO create error msg for when they are trying to remove when cart is empty */
         if (session.getAttribute("cart") != null) {
             cart = (ArrayList<Item>)session.getAttribute("cart");
-            // doesn't work
-//            session.removeAttribute("cart");
         }
         else {
             cart = new ArrayList<>();
         }
 
         String msg = "";
-        List<Integer> idList = null;
         // TODO #1 check to see if items can be found from input received
-        for(String s : markedItem){
-            int id = Integer.parseInt(s);
-            Item item = itemDao.findById(id);
-            if (idList.contains(id)){
-                msg += (item.getName() + " has been found!");
+
+        try {
+
+            for(String s : markedItem){
+                int id = Integer.parseInt(s);
+                for(int i = 0; i < cart.size(); i ++) {
+                    if(cart.get(i).getUid() == id) {
+                        cart.remove(i);
+                    }
+                }
             }
+        } catch(Exception e) {
+            msg += e.getMessage().toString();
         }
+
 
         session.setAttribute("cart", cart);
         model.addAttribute("cart", cart);
@@ -106,8 +111,6 @@ public class EditCartController {
         model.addAttribute("msg", msg);
         session.setAttribute("msg", msg);
 
-        // doesn't work
-//        sessionStatus.setComplete();
         return "redirect:/editCart";
     }
 }
